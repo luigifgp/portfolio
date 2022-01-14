f3f0f4923320f2bcbab03394ef53261bd89e35c4
Luigi Garcia <luigifgp@gmail.com>
2022-01-14 17:01:26 +0000

    .

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -7,19 +7,17 @@ import HamburgerIcon from '../components/Sidebar/HamburgerIcon';
 const HomePage:React.FunctionComponent =() => {
     return (
       //TODO DARKMODE bg-slate-200 dark:
-      
-       
-        <div className="homepage">
-          <Presentation />
-          <p className="mt-7 text-lg sm:text-2x1 md:text-3x1 lg:texl-4xl text-gray-500 italic">
-            Fullstack Developer / React TS
-          </p>
 
-          <button className="button">
-            Contact me!
-          </button>
-        </div>
-     
+      <div className="homepage">
+        <Presentation />
+        <p className="mt-7 text-lg sm:text-2x1 md:text-3x1 lg:texl-4xl text-gray-500 italic">
+          Fullstack Developer / React TS
+        </p>
+        <a href="/contact">
+          <button className="button">  Contact me!</button>
+        
+        </a>
+      </div>
     );
 }
 
